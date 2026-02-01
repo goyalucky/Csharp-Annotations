6d0fc265b277b62ec35527af68b6d18dd323c3d1
goyallucky <luckygoyal221982@gmail.com>
2026-02-01 17:37:19 +0530

    [Lucky] add main program to run all annotation demos

---
+++ Annotations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using OveridingExample;
+using ObseleteAttribute;
+using Warnings;
+using CustomAttribute;
+using BugReport;
+using ImportantMethods;
+using TodoMethod;
+using LogExecutionDemo;
+using MaxLengthAttribute;
+using RoleBasedAccessDemo;
+using CustomJsonSerialization;
+
+namespace Annotations
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            JsonMain.Execute();
+
+            // RoleMain.Execute();
+
+            // MaxLengthMain.Execute();
+
+            // LogMain.Execute();
+
+            // TodoMain.Execute();
+
+            // ImportantMain.Execute();
+
+            // BugReportMain.Execute();
+
+            // CustomMain.Execute();
+
+           /*  WarningDemo w = new WarningDemo();
+            w.UseArrayList(); */
+
+            /*
+            LegacyApi api = new LegacyApi();
+            api.OldFeature();
+            api.NewFeature();
+            */
+
+            /*
+            Dog dog = new Dog();
+            dog.MakeSound();
+            */
+        }
+    }
+}
