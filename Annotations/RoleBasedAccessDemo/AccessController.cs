87e4b915350fe25010b8dcdea351ea60ec49b707
goyallucky <luckygoyal221982@gmail.com>
2026-02-01 17:18:44 +0530

    [Lucky] add enforce role-based access control using RoleAllowed attribute

---
+++ Annotations/RoleBasedAccessDemo/AccessController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+namespace RoleBasedAccessDemo{
+    public static class AccessController{
+    public static void Invoke(User user, object service, string methodName)
+    {
+        Type type = service.GetType();
+        MethodInfo method = type.GetMethod(methodName);
+
+        if (method == null)
+        {
+            Console.WriteLine("Method not found!");
+            return;
+        }
+
+        var roleAttr = (RoleAllowedAttribute)
+            Attribute.GetCustomAttribute(type, typeof(RoleAllowedAttribute));
+
+        if (roleAttr != null && roleAttr.Role != user.Role)
+        {
+            Console.WriteLine("Access Denied!");
+            return;
+        }
+        method.Invoke(service, null);
+    }
+}
+}
